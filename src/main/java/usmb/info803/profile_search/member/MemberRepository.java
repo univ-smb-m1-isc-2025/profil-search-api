f189309524bc25c33f1e8f66f59990628e5c0c03
osteriux <pixave73@gmail.com>
2025-04-18 10:24:03 +0200

    fix rm token

--- src/main/java/usmb/info803/profile_search/member/MemberRepository.java
+++ src/main/java/usmb/info803/profile_search/member/MemberRepository.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByEmail(String email);
-    Member findByToken(String token);
     List<Member> findByNomAndPrenom(String nom, String prenom);
     List<Member> findByActif(boolean actif);
     List<Member> findByEntrepriseId(Long entrepriseId);
